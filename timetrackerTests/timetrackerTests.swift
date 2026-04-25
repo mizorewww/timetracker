df407f79c22907a9e48abf4667a6a7b782fdeab8
mizorewww <kn4jvhg4r2@privaterelay.appleid.com>
2026-04-25 17:45:29 +0800

    Initial Commit

---
+++ timetrackerTests/timetrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  timetrackerTests.swift
+//  timetrackerTests
+//
+//  Created by gaozexuan on 2026/4/25.
+//
+
+import Testing
+
+struct timetrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
